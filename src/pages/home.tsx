345e422b3a065dad5817b6797ee93e2439fd51f8
Ilia Levchenko <i.levchenko@lampalmpa.net>
2024-12-09 23:26:54 +0200

    feature/routing: Added main logic for routing

---
+++ src/pages/home.tsx
@@ -0,0 +1,22 @@
+import { Container } from '@shared/ui/container';
+import { Section } from '@shared/ui/section';
+
+export const Home = () => (
+  <div>
+    <Section>
+      <Container>Section 1</Container>
+    </Section>
+    <Section>
+      <Container>Section 2</Container>
+    </Section>
+    <Section>
+      <Container>Section 3</Container>
+    </Section>
+    <Section>
+      <Container>Section 4</Container>
+    </Section>
+    <Section>
+      <Container>Section 5</Container>
+    </Section>
+  </div>
+);
