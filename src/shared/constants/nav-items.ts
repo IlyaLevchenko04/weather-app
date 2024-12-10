b195427469ff212af7d695801d1386b4b9418c24
Ilia Levchenko <i.levchenko@lampalmpa.net>
2024-12-10 21:50:08 +0200

    feature/header: Added header component

--- src/shared/constants/nav-items.ts
+++ src/shared/constants/nav-items.ts
@@ -1,8 +1,15 @@
 import { ROUTER_BOOK } from './router-book';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
 export const NAV_ITEMS = [
   {
     title: 'Home',
     link: ROUTER_BOOK.index,
+    icon: null,
+  },
+  {
+    title: '',
+    link: ROUTER_BOOK.favorite,
+    icon: FavoriteBorderOutlinedIcon,
   },
 ];
