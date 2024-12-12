ec9a14d0b2df803a04906cbe4ffcf336116fd57f
Ilia Levchenko <i.levchenko@lampalmpa.net>
2024-12-12 04:51:13 +0200

    url

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,10 @@ import App from './App.tsx';
 import { Provider } from 'react-redux';
 import { store } from '@redux/store.ts';
 
-const baseUrl = () =>
-  import.meta.env.VITE_NODE_ENV === 'dev' ? '/' : '/weather-app/';
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename={baseUrl()}>
+      <BrowserRouter basename={'/'}>
         <App />
       </BrowserRouter>
     </Provider>
